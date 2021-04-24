82a5c0bf4f753a5cb111f104b946e9d478b5a3aa
Gy <406631892@qq.com>
2021-04-24 11:49:50 +0800

    完成TopNav Doc aside 的样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>UI</h1>
       <h2>一个厉害的UI</h2>
       <p class="actions">
-        <a href="">github</a>
-        <a href="">开始</a>
+        <a href="https://github.com">github</a>
+        <router-link to="/Doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -24,4 +24,25 @@
 
 <style lang="scss" scoped>
   .topNav{border: 1px solid red;}
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+      padding: 8px 0;
+      a {
+        margin:0 8px;
+        background: #fff;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
+    }
+  }
 </style>
