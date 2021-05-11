7b6b49599feaab0ddd56e7cdd0ef9474791cc110
Gy <406631892@qq.com>
2021-05-11 23:28:04 +0800

    创建Dialog组件

--- src/components/DialogPage.vue
+++ src/components/DialogPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    Dialog的文档
-  </div>
+  <h1>Dialog实例</h1>
+  <h2>实例1</h2>
+  <Dialog />
 </template>
+
+<script lang="ts">
+  import Dialog from '../lib/Dialog.vue'
+  export default {
+    components:{Dialog}
+  }
+</script>
