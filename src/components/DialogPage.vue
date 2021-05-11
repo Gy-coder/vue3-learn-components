a7108bd002a62f407ef4ca6ede8dff6b9b9a18a9
Gy <406631892@qq.com>
2021-05-12 00:31:12 +0800

    Dialog组件支持点击自定义title和content

--- src/components/DialogPage.vue
+++ src/components/DialogPage.vue
@@ -2,7 +2,15 @@
   <h1>Dialog实例</h1>
   <h2>实例1</h2>
   <Button :onclick="handleClick">show dialog</Button>
-  <Dialog v-model:visible="x" :ok="f1" :cancel="f2"/>
+  <Dialog v-model:visible="x" :ok="f1" :cancel="f2" :close-on-click-overlay="false">
+    <template v-slot:title>
+      <strong>厉害的开头</strong>
+    </template>
+    <template v-slot:content>
+      <div>厉害的第一条</div>
+      <div>厉害的第二条</div>
+    </template>
+  </Dialog>
 </template>
 
 <script lang="ts">
@@ -16,7 +24,7 @@
         x.value = !x.value
       }
       const f1 = ()=>{
-
+        console.log('ok')
       }
       const f2 = ()=>{
         console.log('cancel')
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -4,12 +4,11 @@
     <div class="g-dialog-wrapper">
       <div class="g-dialog">
         <header>
-          标题
+          <slot name="title" />
           <span class="g-dialog-close" @click="close"></span>
         </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot name="content" />
         </main>
         <footer>
           <Button @click="ok">OK</Button>
