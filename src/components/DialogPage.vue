0fe6483a890a07af12225ca7825ba5fb428a36ce
Gy <406631892@qq.com>
2021-05-11 23:41:55 +0800

    Dialog组件支持visible

--- src/components/DialogPage.vue
+++ src/components/DialogPage.vue
@@ -1,12 +1,22 @@
 <template>
   <h1>Dialog实例</h1>
   <h2>实例1</h2>
-  <Dialog />
+  <Button :onclick="handleClick">show dialog</Button>
+  <Dialog :visible="x"/>
 </template>
 
 <script lang="ts">
+  import {ref} from 'vue'
   import Dialog from '../lib/Dialog.vue'
+  import Button from '../lib/Button.vue';
   export default {
-    components:{Dialog}
+    setup(){
+      const x = ref(false)
+      const handleClick = ()=>{
+        x.value = !x.value
+      }
+      return {x,handleClick}
+    },
+    components:{Dialog,Button}
   }
 </script>
